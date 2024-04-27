b0f12ead479c46cb9ac542f809ea7dcfec15be6c
Ticopera20 <luis_ticona1@usmp.pe>
2024-04-27 11:06:57 -0500

    Primer Commint

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ParcialTicona.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
